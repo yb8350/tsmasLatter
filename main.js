62c18c51d8d8e53cb7481998dd2d1a721919340d
yb8350 <yb8350@naver.com>
2021-12-25 03:39:28 +0900

    크리스마스 초대장

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ let doorEl = document.querySelector(".door");
 let popupEl = document.querySelector(".popup");
 let blackEl = document.querySelector(".bg_black");
 doorEl.addEventListener("click", function () {
-  audio.play();
-  popupEl.style.display = "flex";
-  blackEl.style.display = "block";
+  doorEl.style.transform = "rotateY(180deg)";
+  doorEl.style.transformOrigin = "left";
+  setTimeout(function () {
+    audio.loop = true;
+    audio.play();
+    popupEl.style.display = "flex";
+    blackEl.style.display = "block";
+  }, 800);
 });
 
 document.querySelector(".close").addEventListener("click", function () {
+  doorEl.style.transform = "rotateY(0deg)";
+  doorEl.style.transformOrigin = "left";
   audio.pause();
   popupEl.style.display = "none";
   blackEl.style.display = "none";
